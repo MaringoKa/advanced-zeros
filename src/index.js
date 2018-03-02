009c6ccc34986e07cf07c13e59059a8047434d76
MaringoKa <centrenrr@gmail.com>
2018-03-02 16:25:31 +0200

    //

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
-}
+    var count = 0;
+    var zero = Number.MAX_VALUE;
+
+      for (var i = 2; i <= base; i++) {
+        if (base % i == 0) {
+              count = 0;
+              while (base % i == 0) {
+                count++;
+                base = base / i;
+            }
+              var count1 = 0;
+              var count2 = number / i;
+                while (count2 > 0) {
+                   count1 = count1 + count2;
+                   count2 = Math.floor(count2 / i);
+            }
+            zero = Math.min(zero, Math.floor(count1 / count));
+        }
+    }
+   return zero;
+};
